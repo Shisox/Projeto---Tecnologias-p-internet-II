6be9c48b9737eb7e8b04302810462cbafdb6161f
Pedro Talala <pedrop.talala@gmail.com>
2026-03-31 16:25:06 -0300

    Css simplificado

--- php/index.php
+++ php/index.php
@@ -3,14 +3,16 @@
     <head>
         <title>Site</title>
     </head>
-        <body>
-            <div class="login-container">
-                <h1>Site</h1>
-                <form method="post" action="login.php">
-                    CPF: <input type="text" name="cpf"> <br>
-                    SENHA: <input type="password" name="senha"> <br>
-                    <input type="submit" value="Enviar">
-                </form>
-            </div>
-        </body>
+    <body>
+        <div class="login-container">
+            <h1>Site</h1>
+            <form method="post" action="login.php">
+                CPF:<br>
+                <input type="text" name="cpf"><br>
+                SENHA:<br>
+                <input type="password" name="senha"><br>
+                <input type="submit" value="Enviar">
+            </form>
+        </div>
+    </body>
 </html>
--- php/principal.php
+++ php/principal.php
@@ -7,20 +7,17 @@
         <title>Site</title>
     </head>
     <body>
-        <div class="dashboard">
-            <div class="dashboard-header">
-                <div class="welcome">Olá, <?php echo $_SESSION['nome']; ?>!</div>
-                <a href="sair.php" class="logout-btn">Sair</a>
-            </div>
-            
-            <div class="dashboard-main">
-                <div class="dashboard-menu">
-                    Menu
-                </div>
-                <div class="dashboard-content">
-                    Conteúdo
-                </div>
-            </div>
-        </div>
+        <header>
+            <span>Olá, <?php echo $_SESSION['nome']; ?>!</span>
+            <a href="sair.php">Sair</a>
+        </header>
+        
+        <aside>
+            Menu
+        </aside>
+        
+        <main>
+            Conteúdo
+        </main>
     </body>
 </html>

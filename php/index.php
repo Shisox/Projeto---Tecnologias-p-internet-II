b23cb6800e4ed3b30e0b509a926d905b41412708
Pedro Talala <pedrop.talala@gmail.com>
2026-03-31 16:37:14 -0300

    aplicação básica 23/03

--- php/index.php
+++ php/index.php
@@ -4,13 +4,11 @@
         <title>Site</title>
     </head>
         <body>
-            <div class="login-container">
-                <h1>Site</h1>
-                <form method="post" action="login.php">
-                    CPF: <input type="text" name="cpf"> <br>
-                    SENHA: <input type="password" name="senha"> <br>
-                    <input type="submit" value="Enviar">
-                </form>
-            </div>
+            <h1>Site 01</h1>
+            <form method="post" action="login.php">
+                CPF: <input type="text" name="cpf"> <br>
+                SENHA: <input type="password" name="senha"> <br>
+                <input type="submit" value="Enviar">
+            </form>
         </body>
 </html>
--- php/principal.php
+++ php/principal.php
@@ -1,26 +1,11 @@
 <?php
     include("valida.php");
+    echo 'Olá, '.$_SESSION['nome'];
 ?>
+
 <html>
     <link rel="stylesheet" href="style.css">
-    <head>
-        <title>Site</title>
-    </head>
-    <body>
-        <div class="dashboard">
-            <div class="dashboard-header">
-                <div class="welcome">Olá, <?php echo $_SESSION['nome']; ?>!</div>
-                <a href="sair.php" class="logout-btn">Sair</a>
-            </div>
-            
-            <div class="dashboard-main">
-                <div class="dashboard-menu">
-                    Menu
-                </div>
-                <div class="dashboard-content">
-                    Conteúdo
-                </div>
-            </div>
-        </div>
-    </body>
 </html>
+
+<hr>
+<a href="sair.php">Sair</a>
